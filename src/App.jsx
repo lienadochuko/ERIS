c4686d0f30625d4dec0e8c1810d94a167ef97c66
lienadochuko <erhisowomedanielking@gmail.com>
2022-10-07 08:41:09 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import { useContext } from "react";
+import React,{ useContext, useRef, useState } from "react";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Toggel from "./components/toggle/Toggel";
 import { ThemeContext } from "./context";
-
+import './components/toggle/toggle.css';
 
 const App = () => {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
 
-  const julie = document.getElementById('julie');
+  /*const scrollRef = document.getElementById('julie'); 
+    useEffect(() => {
+        scrollRef.julie?.scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+            inline: "nearest"
+        })
+    }, [])*/
+
+  const julie = document.getElementById('julie'); 
   const dave = (e) => { 
     e.preventDefault();
+    julie.scrollIntoView();
     julie.scrollIntoView(true);
+    julie.scrollIntoView({block: "start"});
+    julie.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
   }
 
   const steve = document.getElementById('steve');
@@ -39,12 +51,14 @@ const App = () => {
   <div style={{backgroundColor:darkMode ? "#222" : "white", 
   color:darkMode && "white" }} >
      <div className='tmenu' 
-     style={{color:darkMode ? "black" : "white" , background:darkMode ? "rgba(255, 255, 255, 0.447)" : "rgba(0, 0, 0, 0.486"}}>
-      <p className='p2' onClick={dave}>Home</p>
-      <p className='p1' onClick={david}>About</p>
-      <p className='p1' onClick={atony}>Product</p>
-      <p className='p3' onClick={bian}>Contact</p>
-      <Toggel/></div>
+     style={{color:darkMode ? "black" : "white" , 
+     background:darkMode ? "rgba(255, 255, 255, 0.447)" : "rgba(0, 0, 0, 0.486"}}>
+      <div className='p2' onClick={dave}>Home</div>
+      <div className='p1' onClick={david}>About</div>
+      <div className='p1' onClick={atony}>Product</div>
+      <div className='p3' onClick={bian}>Contact</div>
+      <Toggel/>
+      </div>
     <section id='julie'>
     <Intro/>
     </section>
