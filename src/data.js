fe43847132e4f77a34bf01c2b50685483d198762
erhis daniel <66309753+lienadochuko@users.noreply.github.com>
2023-03-15 10:51:48 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,8 +2,7 @@ export const products = [
     {  
      id: 4,
      img: " https://user-images.githubusercontent.com/66309753/183740094-9c2492ba-6687-437a-b339-ba3c9abbe557.jpg",
-     link: "https://erhisdaniel.netlify.app/",
-    },
+     link: "https://www.behance.net/gallery/134098691/Monster-music",
     {
      id: 5,
      img: "https://user-images.githubusercontent.com/66309753/225267317-c9009bf3-1787-47c3-8e72-f0ad773842ff.png",
@@ -12,21 +11,22 @@ export const products = [
     {
       id: 6,
       img: "https://user-images.githubusercontent.com/66309753/196287094-e370a961-1ded-40cd-ae2b-dc3bf040cbab.png",
-      link: "http://eris.dev",
+      link: "https://getcountryinfo.netlify.app/",
      },
     {
       id: 1,
       img: "https://user-images.githubusercontent.com/66309753/183770995-bbc4903f-c7be-45da-ad6d-34ddd851419f.png",
-      link: "https://www.behance.net/gallery/134098691/Monster-music",
+      link: "https://erhisdaniel.netlify.app/",
+    },
     },
     {
       id: 2,
-      img: "https://user-images.githubusercontent.com/66309753/183708776-9e6a6f7e-4ead-476d-a4cd-4e401d6e066d.png",
+      img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/bc9c1f143800479.6281681f96261.png",
       link: "https://www.behance.net/gallery/143800479/SphereBank-UIUX",
     },
     {
       id: 3,
-      img: "https://user-images.githubusercontent.com/66309753/225267813-5f170c0c-633d-446b-8375-aa52ae36e63c.png",
-      link: "https://getcountryinfo.netlify.app/",
+      img: "https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/540887133647453.61c25cbbe265d.png",
+      link: "http://eris.dev",
     },
 ];
